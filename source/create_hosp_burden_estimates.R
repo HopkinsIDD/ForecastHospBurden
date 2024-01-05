839533a7e92eec25db83a14b6bfd86f442ce510c
cottonsarah <79435516+cottonsarah@users.noreply.github.com>
2024-01-05 14:25:13 -0700

    wrote functions (create_hosp_dates & create_curr_hosp) for empirical and ensemble data, updated notes on function uses

--- source/create_hosp_burden_estimates.R
+++ source/create_hosp_burden_estimates.R
@@ -79,49 +79,55 @@ burden_est_funct <- function(incidH, date, hospstayfunct = covidhosp_stay_funct)
 # ~ COVID-19 ensemble data --------------------------------------------------------------
 
 # create function for selecting state & scenario
-# df returns median incidence hosp 
+# function requires code for state and scenario id from covid19-scenario-modeling-hub Ensemble_LOP file 2023-04-16
+# returns data frame for specific state (ie: NJ) and scenario (ie: A-2023-04-16) with projection date and median incidence hospitalizations
 select_parameters <- function(state, scenario){
   parameters_ensemble_data <- ensemble_data %>%
     filter(location == state,
            scenario_id == scenario) %>%
-    group_by(horizon, scenario_id, target, origin_date, location, type) %>%
-    summarize(mdn_incidH = median(value)) %>% 
-    # convert horizon to date for burden_est_funct
-    mutate(date = as_date(origin_date + horizon*7))
+    mutate(date = as_date(origin_date + horizon*7)) %>% 
+    group_by(date, scenario_id, target, origin_date, location, type) %>%
+    # update var name to incidH for hosp functions
+    summarize(incidH = median(value)) %>% 
+    # convert horizon to date for burden_est_funct %>% 
+    # keeping horizon made issue in create_curr_hosp
+
+    
   return(parameters_ensemble_data)
 }
 
 
 #filter state to NJ, scenario to A 
-
 NJ_A_ensemble_data <- select_parameters(state = "34", scenario = "A-2023-04-16")
 
+
+
 # creating current hosp of ensemble data 
-NJ_A_ensemble_data_burden <- list()
+# NJ_A_ensemble_data_burden <- list()
+# 
+# for (i in 1:nrow(NJ_A_ensemble_data)){
+#   
+#   NJ_A_ensemble_data_burden[[i]] <- NJ_A_ensemble_data[i, ] %>%
+#     # need to think about date 
+#     rename(admit_date = date) %>%
+#     expand_grid(hosp_dates = 
+#                   burden_est_funct(incidH = NJ_A_ensemble_data$mdn_incidH[i], 
+#                                    date = NJ_A_ensemble_data$date[i], 
+#                                    hospstayfunct = covidhosp_stay_funct)
+#     )
+# }
 
-for (i in 1:nrow(NJ_A_ensemble_data)){
-  
-  NJ_A_ensemble_data_burden[[i]] <- NJ_A_ensemble_data[i, ] %>%
-    # need to think about date 
-    rename(admit_date = date) %>%
-    expand_grid(hosp_dates = 
-                  burden_est_funct(incidH = NJ_A_ensemble_data$mdn_incidH[i], 
-                                   date = NJ_A_ensemble_data$date[i], 
-                                   hospstayfunct = covidhosp_stay_funct)
-    )
-}
-
-NJ_A_ensemble_data_burden_covid <- NJ_A_ensemble_data_burden %>%
-  bind_rows() %>%
-  select(-admit_date, -mdn_incidH) %>%
-  group_by(location, scenario_id, origin_date, type, hosp_dates) %>%
-  summarise(curr_hosp = length(hosp_dates)) %>%
-  ungroup()
-
-NJ_A_ensemble_data_burden_covid %>%
-  ggplot(aes(x = hosp_dates, y = curr_hosp)) + 
-  geom_line() 
-
+# NJ_A_ensemble_data_burden_covid <- NJ_A_ensemble_data_burden %>%
+#   bind_rows() %>%
+#   select(-admit_date, -mdn_incidH) %>%
+#   group_by(location, scenario_id, origin_date, type, hosp_dates) %>%
+#   summarise(curr_hosp = length(hosp_dates)) %>%
+#   ungroup()
+# 
+# NJ_A_ensemble_data_burden_covid %>%
+#   ggplot(aes(x = hosp_dates, y = curr_hosp)) + 
+#   geom_line() 
+# 
 
 # ~ COVID-19 --------------------------------------------------------------
 
@@ -129,30 +135,76 @@ covid_data <- nj_data %>%
     filter(pathogen == "COVID-19") %>%
     filter(!is.na(incidH) & incidH>0)
 
-nj_data_burden <- list()
-for (i in 1:nrow(covid_data)){
+
+# create hosp_dates function for empirical and ensemble data 
+# function requires dataframe with incidence Hosp of COVID-19 (empirical or ensemble) 
+# returns list with hosp_dates to be used in create_curr_hosp function 
+create_hosp_dates <- function(data){
+  data_burden <- list()
+  
+  for (i in 1:nrow(data)){
     
-    nj_data_burden[[i]] <- covid_data[i, ] %>%
-        rename(admit_date = date) %>%
-        expand_grid(hosp_dates = 
-            burden_est_funct(incidH = covid_data$incidH[i], 
-                             date = covid_data$date[i], 
-                             hospstayfunct = covidhosp_stay_funct)
-        )
+    data_burden[[i]] <- data[i, ] %>%
+      rename(admit_date = date) %>%
+      expand_grid(hosp_dates = 
+                    burden_est_funct(incidH = data$incidH[i], 
+                                     date = data$date[i], 
+                                     hospstayfunct = covidhosp_stay_funct)
+      )
+  }
+  return(data_burden)
 }
 
+nj_data_burden <- create_hosp_dates(data = covid_data) 
+NJ_A_ensemble_data_burden <- create_hosp_dates(data = NJ_A_ensemble_data)
 
-
-nj_data_burden_covid <- nj_data_burden %>%
+# current hospitalization function for empirical and ensemble data 
+# function requires data burden list from create_hosp_dates function 
+# returns dataframe with current hospitalizations with (empirical or ensemble data)
+create_curr_hosp <- function(data_burden){
+  new_data_burden <- data_burden %>%
     bind_rows() %>%
     select(-admit_date, -incidH) %>%
-    group_by(source, FIPS, pathogen, hosp_dates) %>%
+    group_by_all() %>%
     summarise(curr_hosp = length(hosp_dates)) %>%
     ungroup()
+  return(new_data_burden)
+}
+
+nj_data_burden_covid <- create_curr_hosp(data_burden = nj_data_burden)
+NJ_A_ensemble_data_burden_covid <- create_curr_hosp(data_burden = NJ_A_ensemble_data_burden)
+
+# visualization
 
 nj_data_burden_covid %>%
-  ggplot(aes(x = hosp_dates, y = curr_hosp)) + 
-  geom_line() 
+  ggplot(aes(x = hosp_dates, y = curr_hosp)) +
+  geom_line()
+
+NJ_A_ensemble_data_burden_covid %>%
+  ggplot(aes(x = hosp_dates, y = curr_hosp)) +
+  geom_line()
+
+# nj_data_burden <- list()
+# for (i in 1:nrow(covid_data)){
+# 
+#   nj_data_burden[[i]] <- covid_data[i, ] %>%
+#     rename(admit_date = date) %>%
+#     expand_grid(hosp_dates =
+#                   burden_est_funct(incidH = covid_data$incidH[i],
+#                                    date = covid_data$date[i],
+#                                    hospstayfunct = covidhosp_stay_funct)
+#     )
+# }
+# nj_data_burden_covid <- nj_data_burden %>%
+#     bind_rows() %>%
+#     select(-admit_date, -incidH) %>%
+#     group_by(source, FIPS, pathogen, hosp_dates) %>%
+#     summarise(curr_hosp = length(hosp_dates)) %>%
+#     ungroup()
+# 
+# nj_data_burden_covid %>%
+#   ggplot(aes(x = hosp_dates, y = curr_hosp)) +
+#   geom_line()
 
 # ~ Influenza --------------------------------------------------------------
 
