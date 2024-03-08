c8aa2637edaffe40961509a75f895031685c249d
cottonsarah <79435516+cottonsarah@users.noreply.github.com>
2024-03-08 08:36:23 -0500

    import new data, export subset of hospitalizations

--- source/write_census_current_hosp_csv_to_parquet.R
+++ source/write_census_current_hosp_csv_to_parquet.R
@@ -1,6 +1,8 @@
-
 library(tidyverse)
 library(arrow)
+library(gtable)
+library(grid)
+library(gridExtra)
 
 # # create state_files list for loop 
 # # Updated state_files list with specified path structure
@@ -27,3 +29,44 @@ library(arrow)
 #   # Write to parquet file
 #   write_parquet(data, paste0("data/currently_hosp_covid19_by_state_parquet/", state, "_currently_hospitalized_covid19_patients.parquet"))
 # }
+
+raw_covid_totalHosp <- read_csv("data/currently_hosp_covid_data_daily/COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_State_Timeseries__RAW__20240306.csv")
+
+clean_covid_totalHosp <- raw_covid_totalHosp %>% 
+  dplyr::select(state, date, inpatient_beds, inpatient_beds_coverage, inpatient_beds_used, inpatient_beds_used_coverage,
+                inpatient_beds_used_covid, inpatient_beds_used_covid_coverage, 
+                total_adult_patients_hospitalized_confirmed_and_suspected_covid, total_adult_patients_hospitalized_confirmed_and_suspected_covid_coverage,
+                total_adult_patients_hospitalized_confirmed_covid, total_adult_patients_hospitalized_confirmed_covid_coverage,
+                total_pediatric_patients_hospitalized_confirmed_and_suspected_covid, total_pediatric_patients_hospitalized_confirmed_and_suspected_covid_coverage,
+                total_pediatric_patients_hospitalized_confirmed_covid, total_pediatric_patients_hospitalized_confirmed_covid_coverage)
+
+
+state_sample_covid_totalHosp <- clean_covid_totalHosp %>% 
+  filter(state %in% c("NJ", "MD", "PA", "NY")) 
+
+write_parquet(state_sample_covid_totalHosp, "data/currently_hosp_covid_data_daily/COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_State_Timeseries_Subset.parquet")
+# Check total COVID hospitalizations 
+
+state_counts <- state_sample_covid_totalHosp %>%
+  mutate(check_patient_count = if_else(total_adult_patients_hospitalized_confirmed_and_suspected_covid +
+                                         total_pediatric_patients_hospitalized_confirmed_and_suspected_covid ==
+                                         inpatient_beds_used_covid,
+                                       TRUE, FALSE)) %>% 
+  group_by(state) %>%
+  summarize(TRUE_count = sum(check_patient_count == TRUE, na.rm = TRUE),
+            FALSE_count = sum(check_patient_count == FALSE, na.rm = TRUE))
+
+
+table <- cbind(state_counts$state, state_counts$TRUE_count, state_counts$FALSE_count)
+colnames(table) <- c("State", "TRUE Count", "FALSE Count")
+
+# Create a gtable object
+table_g <- tableGrob(table)
+
+# Draw the table
+grid.newpage()
+grid.draw(table_g)
+
+
+
+
