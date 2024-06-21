b1b8bac6801b89ff8f300bc22e75e46f122b78a1
Sarah Cotton <scotton6@jh.edu>
2024-06-21 11:24:55 -0600

    updating source and create hosp scripts

--- source/create_hosp_burden_estimates.R
+++ source/create_hosp_burden_estimates.R
@@ -6,15 +6,44 @@ library(tidycensus)
 library(tidyverse)
 library(readr)
 library(lubridate)
-library(flepicommon)
+library(gghighlight)
+library(arrow)
+library(Hmisc)
 
+### IMPORT INITIAL DATA -----------------------------------
 
 # source data functions
 source("source/data_setup_source.R")
 
 opt <- list()
-opt$gt_data_source <- "hhs_hosp"
-opt$delphi_api_key <- "04e7369e1541a"
-opt$gt_data_path <- "data/nj_covid_hosp.parquet"
+#opt$gt_data_source <- "hhs_hosp"
+#opt$delphi_api_key <- "04e7369e1541a"
+opt$gt_data_path <- "data/US_wide_data/COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_State_Timeseries_All_States_06-07-2024.parquet"
 
-source("source/pull_empirical_data.R")
+#source("source/pull_empirical_data.R") # create script for reading in initial code and update here
+
+# read in data, define total_hosp, incidH_prior_day
+covid_HHS_data_states <- arrow::read_parquet(opt$gt_data_path) %>% 
+  mutate(total_hosp = total_adult_patients_hospitalized_confirmed_covid + total_pediatric_patients_hospitalized_confirmed_covid,
+         incidH_prior_day = previous_day_admission_adult_covid_confirmed + previous_day_admission_pediatric_covid_confirmed,
+         date = as_date(date)) %>% 
+  arrange(state, date) %>% 
+  dplyr::select(state, date, total_hosp, incidH_prior_day)
+
+# add labels to new variables 
+attr(covid_HHS_data_states[["total_hosp"]], "label") <- "total_adult_patients_hospitalized_confirmed_covid + total_pediatric_patients_hospitalized_confirmed_covid"
+attr(covid_HHS_data_states[["incidH_prior_day"]], "label") <- "previous_day_admission_adult_covid_confirmed + previous_day_admission_pediatric_covid_confirmed"
+
+### SETUP FILES FOR EACH STATE TO ESTIMATE HOSPITAL BURDEN -----------------------------------
+
+# Lag incident hospitalizations 
+## incidH_prior_day is moved back one day and renamed incidH
+covid_HHS_data_states_lag <- create_incidH_lag(covid_HHS_data_states) 
+
+# add labels 
+attr(covid_HHS_data_states_lag[["incidH"]], "label") <- "lead(previous_day_admission_adult_covid_confirmed + previous_day_admission_pediatric_covid_confirmed)"
+attr(covid_HHS_data_states_lag[["total_hosp"]], "label") <- "total_adult_patients_hospitalized_confirmed_covid + total_pediatric_patients_hospitalized_confirmed_covid"
+attr(covid_HHS_data_states_lag[["incidH_prior_day"]], "label") <- "previous_day_admission_adult_covid_confirmed + previous_day_admission_pediatric_covid_confirmed"
+
+# create file with reported incident data for Table 1 
+write_parquet(covid_HHS_data_states_lag, "data/US_wide_data/State_incidH_table1/covid_HHS_data_states_lag.parquet")
--- source/data_setup_source.R
+++ source/data_setup_source.R
@@ -1,6 +1,7 @@
 # Setup Functions ---------------------------------------------
 
-# Create lag of one day for incidH for each state
+# Create lag of one day for incidH for each state  --------
+## notes: incident data is reported for the day prior, so need to offset by one day
 
 create_incidH_lag <- function(state_data){
   states_list <- unique(state_data$state)
@@ -19,4 +20,18 @@ create_incidH_lag <- function(state_data){
   merged_df <- do.call(bind_rows, lagged_dfs) #combine into one df 
   
   return(merged_df)
+}
+
+# Create dataframes for each state with Hospital burden data -----------------------------------
+
+create_totalH_df <- function(data, state){
+  states_list <- unique(data$state)
+  
+  for (state in states_list) {
+    
+    state_data <- data[data$state == state, ]
+    
+    # put new df in global env 
+    assign(paste0("covid_totalHosp_data_", state), state_data, envir = .GlobalEnv)
+  }
 }
