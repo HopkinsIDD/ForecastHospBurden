c70800228bd1269c3a5630551a6b11d349813388
Sarah Cotton <scotton6@jh.edu>
2024-06-21 16:19:47 -0600

    finalize source and create hosp burden estimate files

--- source/create_hosp_burden_estimates.R
+++ source/create_hosp_burden_estimates.R
@@ -34,7 +34,7 @@ covid_HHS_data_states <- arrow::read_parquet(opt$gt_data_path) %>%
 attr(covid_HHS_data_states[["total_hosp"]], "label") <- "total_adult_patients_hospitalized_confirmed_covid + total_pediatric_patients_hospitalized_confirmed_covid"
 attr(covid_HHS_data_states[["incidH_prior_day"]], "label") <- "previous_day_admission_adult_covid_confirmed + previous_day_admission_pediatric_covid_confirmed"
 
-### SETUP FILES FOR EACH STATE TO ESTIMATE HOSPITAL BURDEN -----------------------------------
+# SETUP FILES FOR EACH STATE TO ESTIMATE HOSPITAL BURDEN -----------------------------------
 
 # Lag incident hospitalizations 
 ## incidH_prior_day is moved back one day and renamed incidH
@@ -47,3 +47,54 @@ attr(covid_HHS_data_states_lag[["incidH_prior_day"]], "label") <- "previous_day_
 
 # create file with reported incident data for Table 1 
 write_parquet(covid_HHS_data_states_lag, "data/US_wide_data/State_incidH_table1/covid_HHS_data_states_lag.parquet")
+
+# Create dataframes for each state with Hospital burden data 
+create_totalH_df(data = covid_HHS_data_states_lag %>% dplyr::select(-incidH, -incidH_prior_day), state) 
+
+# Create dataframes for each state with incident hospitalizations
+create_incidH_df(data = covid_HHS_data_states_lag %>% dplyr::select(-total_hosp, -incidH_prior_day), state)
+
+
+# Check lagged incidH and totalHosp master and state dataframes --------------------------------------------------------------
+
+covid_HHS_data_states_lag %>%
+  ggplot(aes(x = date, y = incidH, color = state)) + 
+  geom_line() 
+
+covid_HHS_data_states_lag %>%
+  ggplot(aes(x = date, y = total_hosp, color = state)) + 
+  geom_line() 
+
+covid_incidH_data_MD %>%
+  ggplot(aes(x = date, y = incidH, color = state)) + 
+  geom_line() 
+
+covid_totalHosp_data_MD %>%
+  ggplot(aes(x = date, y = total_hosp, color = state)) + 
+  geom_line() 
+
+
+# Estimate LOS value for each state using optimization 
+
+create_optimization(parent_data = covid_HHS_data_states_lag, optimize_los)
+# note: parent data just for getting list of all states
+
+write_parquet(los_opt_by_state, "data/US_wide_data/LOS_Optimized_by_AllStates.parquet")
+write_csv(los_opt_by_state, "data/US_wide_data/LOS_Optimized_by_AllStates.csv")
+
+# Write Final files ----------------
+covid_joined_totalHosp_state_data <- create_optimize_totalHosp_data(parent_data = covid_HHS_data_states_lag, los_opt_by_state = los_opt_by_state)
+
+covid_HHS_data <- arrow::read_parquet(opt$gt_data_path_HHS_states) %>% 
+  select(state, date, inpatient_beds, `previous_day_admission_adult_covid_confirmed_18-19`, `previous_day_admission_adult_covid_confirmed_20-29`,
+         `previous_day_admission_adult_covid_confirmed_30-39`, `previous_day_admission_adult_covid_confirmed_40-49`, `previous_day_admission_adult_covid_confirmed_50-59`,               
+         `previous_day_admission_adult_covid_confirmed_60-69`, `previous_day_admission_adult_covid_confirmed_70-79`, `previous_day_admission_adult_covid_confirmed_80+`, `previous_day_admission_adult_covid_confirmed_unknown`,
+         `previous_day_admission_pediatric_covid_confirmed_0_4`, `previous_day_admission_pediatric_covid_confirmed_12_17`, `previous_day_admission_pediatric_covid_confirmed_5_11`,
+         `previous_day_admission_pediatric_covid_confirmed_unknown`, `previous_day_admission_pediatric_covid_confirmed`, `previous_day_admission_adult_covid_confirmed`)
+
+
+covid_joined_totalHosp_state_data_los <- inner_join(covid_joined_totalHosp_state_data, los_opt_by_state, by = "state")
+
+covid_joined_totalHosp_state_data_los_demographic <- left_join(covid_joined_totalHosp_state_data_los, covid_HHS_data, c("state", "date"))
+write_parquet(covid_joined_totalHosp_state_data_los_demographic, "data/US_wide_data/estimated_hospitalizations_data/Obs_Exp_totalHosp_daily_06132024.parquet")
+
