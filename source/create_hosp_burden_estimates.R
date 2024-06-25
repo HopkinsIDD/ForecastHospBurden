bc390f21e1f7f23d8ca5df7b17fe96df2c93e665
Sarah Cotton <scotton6@jh.edu>
2024-06-25 09:38:50 -0600

    adding more comments

--- source/create_hosp_burden_estimates.R
+++ source/create_hosp_burden_estimates.R
@@ -18,8 +18,6 @@ source("source/data_setup_source.R")
 opt <- list()
 opt$gt_data_path <- "data/US_wide_data/COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_State_Timeseries_All_States_06-07-2024.parquet"
 
-#source("source/pull_empirical_data.R") # create script for reading in initial code and update here
-
 # read in data, define total_hosp, incidH_prior_day
 covid_HHS_data_states <- arrow::read_parquet(opt$gt_data_path) %>% 
   mutate(total_hosp = total_adult_patients_hospitalized_confirmed_covid + total_pediatric_patients_hospitalized_confirmed_covid,
@@ -35,7 +33,7 @@ attr(covid_HHS_data_states[["incidH_prior_day"]], "label") <- "previous_day_admi
 # SETUP FILES FOR EACH STATE TO ESTIMATE HOSPITAL BURDEN -----------------------------------
 
 # Lag incident hospitalizations 
-## incidH_prior_day is moved back one day and renamed incidH
+## incidH_prior_day values are moved back one day and add to new column incidH
 covid_HHS_data_states_lag <- create_incidH_lag(covid_HHS_data_states) 
 
 # add labels 
@@ -58,17 +56,17 @@ covid_HHS_data_USA_lag <- covid_HHS_data_states_lag %>%
 covid_HHS_data_states_lag <- bind_rows(covid_HHS_data_states_lag, covid_HHS_data_USA_lag)
 
 # create file with reported incident data for Table 1 
-# only run when update 
+# only run when need to update 
 #write_parquet(covid_HHS_data_states_lag, "data/US_wide_data/State_incidH_table1/covid_HHS_data_states_lag.parquet")
 
 # Create dataframes for each state with Hospital burden data 
 create_totalH_df(data = covid_HHS_data_states_lag %>% dplyr::select(-incidH, -incidH_prior_day), state) 
 
-# Create dataframes for each state with incident hospitalizations
+# Create dataframes for each state with incident hospitalization data
 create_incidH_df(data = covid_HHS_data_states_lag %>% dplyr::select(-total_hosp, -incidH_prior_day), state)
 
 
-# Check lagged incidH and totalHosp master and state dataframes --------------------------------------------------------------
+# Check incidH and totalHosp master file and USA dataframes --------------------------------------------------------------
 
 covid_HHS_data_states_lag %>%
   ggplot(aes(x = date, y = incidH, color = state)) + 
@@ -91,7 +89,7 @@ covid_totalHosp_data_USA %>%
 
 # this take a long time to run, prints states in alphabetical in console to check progress 
 create_optimization(parent_data = covid_HHS_data_states_lag, optimize_los) # note: parent data just for getting list of all states
-#los_opt_by_state <- arrow::read_parquet("data/US_wide_data/LOS_Optimized_by_AllStates_USA.parquet")
+#los_opt_by_state <- arrow::read_parquet("data/US_wide_data/LOS_Optimized_by_AllStates_USA.parquet") # if don't want to run, load file directly 
 
 # update only when want to overwrite file 
 #write_parquet(los_opt_by_state, "data/US_wide_data/LOS_Optimized_by_AllStates_USA.parquet")
@@ -102,7 +100,7 @@ create_optimization(parent_data = covid_HHS_data_states_lag, optimize_los) # not
 # runs faster than above function (fewer outputs) 
 covid_joined_totalHosp_state_data <- create_optimize_totalHosp_data(parent_data = covid_HHS_data_states_lag, los_opt_by_state = los_opt_by_state)
 
-# Write Final files ----------------
+# Write Final files for analysis ----------------
 covid_HHS_data <- arrow::read_parquet(opt$gt_data_path) %>% 
   select(state, date, inpatient_beds, `previous_day_admission_adult_covid_confirmed_18-19`, `previous_day_admission_adult_covid_confirmed_20-29`,
          `previous_day_admission_adult_covid_confirmed_30-39`, `previous_day_admission_adult_covid_confirmed_40-49`, `previous_day_admission_adult_covid_confirmed_50-59`,               
